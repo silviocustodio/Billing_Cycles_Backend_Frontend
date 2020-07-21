987aca5eb9ad37a47a61a2204e2d7bc658ed5422
Silvio Custodio <adm.ti.silviocustodio@gmail.com>
2020-07-21 22:06:15 +0100

    [03][Billing_Cycles_Backend_Frontend]-BillingCycle component: Refactoring to Class

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
-export default (props) => (
-  <div>
-    <h1>Ciclo de Pagamento</h1>
-  </div>
-);
+import React, { Component } from 'react';
+
+import ContentHeader from '../common/template/contentHeader';
+import Content from '../common/template/content';
+
+class BillingCycle extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Ciclo de Pagamentos" small="Cadastro" />
+        <Content>Ciclo de Pagamento</Content>
+      </div>
+    );
+  }
+}
+
+export default BillingCycle;
