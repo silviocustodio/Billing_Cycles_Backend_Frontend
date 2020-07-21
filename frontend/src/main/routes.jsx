ec4d83c32b1b4a854269fad03c9808b6a085e80e
Silvio Custodio <adm.ti.silviocustodio@gmail.com>
2020-07-21 21:54:01 +0100

    [02][Billing_Cycles_Backend_Frontend]-added redux in the dashboard component and without redux example

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Router, Route, Redirect, hashHistory } from 'react-router';
+
+//  COM REDUX
 import Dashboard from '../dashboard/dashboard';
+//  SEM REDUX
+// import Dashboard2 from '../dashboard2/dashboard2';
 import BillingCycle from '../billingCycle/billingCycle';
 
 export default (props) => (
   <Router history={hashHistory}>
+    {/* COM REDUX */}
     <Route path="/" component={Dashboard} />
+    {/* SEM REDUX */}
+    {/* <Route path="/" component={Dashboard2} /> */}
     <Route path="/billingCycles" component={BillingCycle} />
     <Redirect from="*" to="/" />
   </Router>
