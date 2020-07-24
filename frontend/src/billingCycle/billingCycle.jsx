99c0c1ca4477930449bed288b6d2e971cfc0071a
Silvio Custodio <adm.ti.silviocustodio@gmail.com>
2020-07-24 21:50:52 +0100

    [04][Billing_Cycles_Backend_Frontend]-Added tabs components

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -2,13 +2,43 @@ import React, { Component } from 'react';
 
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
+import Tabs from '../common/tab/tabs';
+import TabsHeader from '../common/tab/tabsHeader';
+import TabsContent from '../common/tab/tabsContent';
+import TabHeader from '../common/tab/tabHeader';
 
 class BillingCycle extends Component {
   render() {
     return (
       <div>
         <ContentHeader title="Ciclo de Pagamentos" small="Cadastro" />
-        <Content>Ciclo de Pagamento</Content>
+        <Content>
+          <Tabs>
+            <TabsHeader>
+              <TabHeader
+                label="Listar"
+                icon="bars"
+                target="tabList"
+              ></TabHeader>
+              <TabHeader
+                label="Incluir"
+                icon="plus"
+                target="tabCreate"
+              ></TabHeader>
+              <TabHeader
+                label="Alterar"
+                icon="pencil"
+                target="tabUpdate"
+              ></TabHeader>
+              <TabHeader
+                label="Excluir"
+                icon="trash-o"
+                target="tabDelete"
+              ></TabHeader>
+              <TabsContent></TabsContent>
+            </TabsHeader>
+          </Tabs>
+        </Content>
       </div>
     );
   }
