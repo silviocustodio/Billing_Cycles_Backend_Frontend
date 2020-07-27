3d70cba0816ce66dc6c7c7a1a9db7984c7bc8384
Silvio Custodio <adm.ti.silviocustodio@gmail.com>
2020-07-27 22:01:44 +0100

    [05][Billing_Cycles_Backend_Frontend]-Added first action and reducer, connected with redux

--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+// import If from '../operator/if'
+import { selectTab } from './tabActions';
 class TabHeader extends Component {
   render() {
     return (
@@ -7,6 +10,7 @@ class TabHeader extends Component {
         <a
           href="javascript:;"
           data-toggle="tab"
+          onClick={() => this.props.selectTab(this.props.target)}
           data-target={this.props.target}
         >
           <i className={`fa fa-${this.props.icon}`}></i>
@@ -17,4 +21,7 @@ class TabHeader extends Component {
   }
 }
 
-export default TabHeader;
+const mapStateToProps = (state) => ({ tab: state.tab });
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ selectTab }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader);
